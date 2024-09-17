e4f91965c1243c551e6281b4caa354dfd225cd02
NoNamer777 <oscar.wellner@gmail.com>
2024-09-17 20:17:23 +0200

    Adjust default component

--- apps/front-end-angular/src/app/app.component.ts
+++ apps/front-end-angular/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  standalone: true,
+  imports: [RouterModule],
 })
-export class AppComponent {
-  title = 'front-end-angular';
-}
+export class AppComponent {}
